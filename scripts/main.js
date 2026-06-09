86e642f9d3075497939b2d9505001ec8818b3765
GeanoFeeFoundry <phoenixhideoutrecords@gmail.com>
2026-06-09 17:47:34 +0200

    feat: Scaffold module.json and core project structure

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+Hooks.once('init', async function() {
+    console.log("Geano's Ender Chest | Initializing module");
+});
+
+Hooks.once('ready', async function() {
+    console.log("Geano's Ender Chest | Ready");
+});
